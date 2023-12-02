bdf899547f948968f85a9bc4b013ef10319d459a
Jun <junhuito5771@gmail.com>
2023-12-03 00:26:57 +0800

    feat: Add redux for employee

---
+++ src/redux/features/employee/employeeSlice.ts
@@ -0,0 +1,71 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { AppDispatch, RootState } from '@/redux/store';
+import { EmployeeData } from '@/interfaces/employee.interface';
+import { EmployeeStatus } from '@/constants/employee.enum';
+
+const initialState: {
+  data: EmployeeData[]
+} = {
+  data: []
+}
+
+export const employeeSlice = createSlice({
+  name: 'employee',
+  initialState,
+  reducers: {
+    setEmployee: (state, action: PayloadAction<EmployeeData[]>) => {
+      state.data = action.payload;
+    },
+    addEmployee: (state, action: PayloadAction<EmployeeData>) => {
+      state.data.push(action.payload);
+    },
+    updateEmployeeById: (state, action: PayloadAction<EmployeeData>) => {
+      state.data = state.data.map((employee) =>{
+        if (employee.id === action.payload.id) {
+          return {
+            ...employee,
+            ...action.payload,
+          }
+        }
+        return employee;
+      });
+    },
+    deleteEmployeeById: (state, action: PayloadAction<number>) => {
+      state.data = state.data.filter((employee) => employee.id !== action.payload)
+    },
+  },
+})
+
+export const {
+  setEmployee,
+  addEmployee,
+  updateEmployeeById,
+  deleteEmployeeById,
+} = employeeSlice.actions;
+
+export const getEmployee = () => {
+  return async (dispatch: AppDispatch) => {
+    // TODO:: remove mock client data when axios is implemented
+    // mock client data
+    await new Promise((res) => {
+      setTimeout(res, 2000);
+    });
+    dispatch(setEmployee([
+      {
+        id: 1,
+        name: 'John',
+        email: 'john@example.com',
+        status: EmployeeStatus.ACTIVE,
+      },
+      {
+        id: 2,
+        name: 'Ali',
+        email: 'ali@example.com',
+        status: EmployeeStatus.ACTIVE,
+      }
+    ]))
+  }
+}
+
+export const selectEmployee = (state: RootState) => state.employee.data;
